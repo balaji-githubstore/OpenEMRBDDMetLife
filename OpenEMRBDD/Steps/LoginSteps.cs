d935576797191e73aea6c578633efc88723d1a79
balaji-githubstore <balaji0017@gmail.com>
2021-09-13 15:25:01 +0530

    step definitions created for login

---
+++ OpenEMRBDD/Steps/LoginSteps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace OpenEMRBDD.Steps
+{
+    [Binding]
+    public class LoginSteps
+    {
+        [Given(@"I have browser with openemr page")]
+        public void GivenIHaveBrowserWithOpenemrPage()
+        {
+            Console.WriteLine("given - browser launch");
+        }
+        
+        [When(@"I enter username as ""(.*)""")]
+        public void WhenIEnterUsernameAs(string p0)
+        {
+            Console.WriteLine("username");
+        }
+        
+        [When(@"I enter password as ""(.*)""")]
+        public void WhenIEnterPasswordAs(string p0)
+        {
+            
+        }
+        
+        [When(@"I select language as ""(.*)""")]
+        public void WhenISelectLanguageAs(string p0)
+        {
+            
+        }
+        
+        [When(@"I click on login")]
+        public void WhenIClickOnLogin()
+        {
+            Console.WriteLine("login");
+        }
+        
+        [Then(@"I should get access to the portal with title as ""(.*)""")]
+        public void ThenIShouldGetAccessToThePortalWithTitleAs(string p0)
+        {
+            
+        }
+    }
+}
